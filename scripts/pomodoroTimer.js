44616ec4875b6be1f26faa81fc7a66526ada6c3d
henri <henriborgessilva@gmail.com>
2021-08-01 20:57:59 -0300

    play button verification added

--- scripts/pomodoroTimer.js
+++ scripts/pomodoroTimer.js
@@ -6,32 +6,38 @@ const pomodoroTimeInSeconds = minutesToSeconds(25);
 const restTimeInSeconds = minutesToSeconds(5);
 let pomodoro = false;
 let timerDuration = 0;
+let canStart = true;
 phrase.innerHTML = "Hey, click on play to start!";
 
 
 playButton.addEventListener("click", () => {
-    const initialTime = getCurrentDate();
-    if(pomodoro) {
-        pomodoro = false;
-        timerDuration = restTimeInSeconds;
-        bodyTime.innerHTML = secondsToClockFormat(timerDuration);
-        phrase.innerHTML = "You should take a break now!";
-    }
-    else {            notification.play();
-        timerDuration = pomodoroTimeInSeconds;
-        bodyTime.innerHTML = secondsToClockFormat(timerDuration);
-        phrase.innerHTML = "It's time to work!";
-    }
-    let teste = setInterval(()=> {
-        const currentTime = getCurrentDate();
-        const pastTimeInMilliseconds = getPastTimeInMilliseconds(initialTime, currentTime);
-        const pastTimeInSeconds = millisecondsToSeconds(pastTimeInMilliseconds);
-        const remainingTimeInSeconds = timerDuration - pastTimeInSeconds;
-        const remaingngTimeInClockFormat = secondsToClockFormat(remainingTimeInSeconds);
-        bodyTime.innerHTML = remaingngTimeInClockFormat;
-        if(remainingTimeInSeconds == 0) {
-            clearInterval(teste);
+    if(canStart) {
+        const initialTime = getCurrentDate();
+        if(pomodoro) {
+            pomodoro = false;
+            timerDuration = restTimeInSeconds;
+            bodyTime.innerHTML = secondsToClockFormat(timerDuration);
+            phrase.innerHTML = "You should take a break now!";
+        } else {
             notification.play();
+            timerDuration = pomodoroTimeInSeconds;
+            bodyTime.innerHTML = secondsToClockFormat(timerDuration);
+            phrase.innerHTML = "It's time to work!";
         }
-    }, 1000)
+        let teste = setInterval(()=> {
+            canStart = false;
+            const currentTime = getCurrentDate();
+            const pastTimeInMilliseconds = getPastTimeInMilliseconds(initialTime, currentTime);
+            const pastTimeInSeconds = millisecondsToSeconds(pastTimeInMilliseconds);
+            const remainingTimeInSeconds = timerDuration - pastTimeInSeconds;
+            const remaingngTimeInClockFormat = secondsToClockFormat(remainingTimeInSeconds);
+            bodyTime.innerHTML = remaingngTimeInClockFormat;
+            if(remainingTimeInSeconds == 0) {
+                clearInterval(teste);
+                notification.play();
+                canStart = true;
+                pomodoro ? pomodoro = false : pomodoro = true;
+            }
+        }, 1000)
+    }
 })
